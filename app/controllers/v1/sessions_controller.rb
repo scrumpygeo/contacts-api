22c8ed8d5bc7bdd918527e83783c4b8a782c961e
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-23 14:48:15 +0100

    create session

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,6 +1,12 @@
-class v1::Sessions_Controller < ApplicationController
+class V1::SessionsController < ApplicationController
   def create 
-    
+    user = User.where(email: params[:email]).first
+
+    if user.valid_password?(params[:password])
+      render json: user.as_json(only: [:id, :email]), status: :created  # returns 201
+    else
+      head(:unauthorized)
+    end
   end
 
   def destroy 
