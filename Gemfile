76e78322cf7f00864fdf7a114967032489fa9ec8
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-23 17:38:06 +0100

    add authentication_token

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 # gem 'rack-cors'
 gem 'rack-cors', :require => 'rack/cors'
 gem 'devise', '~> 4.7', '>= 4.7.2'
+gem 'simple_token_authentication', '~> 1.17'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class V1::SessionsController < ApplicationController
   def create 
     user = User.where(email: params[:email]).first
 
-    if user.valid_password?(params[:password])
+    if user&.valid_password?(params[:password])
       render json: user.as_json(only: [:id, :email]), status: :created  # returns 201
     else
       head(:unauthorized)
