cb501e579c666b5c48204fb2ef1a6f99864abfff
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-23 11:17:25 +0100

    add rack-cors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # added this as part of the rack-cors gem config
+  Rails.application.config.hosts << "localhost"
 end
