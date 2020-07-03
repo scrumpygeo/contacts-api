907b18e053a73c9ba147267e97ba99d8f2fbcae9
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-07-03 14:50:29 +0100

    AddUserIdToContacts

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,9 +1,14 @@
 class V1::SessionsController < ApplicationController
+  def show 
+    # this is for endpoint to allow verification user signed in .
+    current_user ? head(:ok) : head(:unauthorized)
+  end
+
   def create 
     @user = User.where(email: params[:email]).first
 
     if @user&.valid_password?(params[:password])
-      render :create, status: :created  # returns 201
+      render :create, status: :created  # renders created view and returns 201 status
     else
       head(:unauthorized)
     end
